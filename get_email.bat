01ebb0c0d7fcf3b230d2c28918ae6cf03d4b4c95
Alex <stefanov.alexandre@gmail.com>
2020-06-13 12:05:01 +0300

    Initial commit

---
+++ get_email.bat
@@ -0,0 +1,2 @@
+CALL %windir%\System32\cmd.exe "/K" C:\ProgramData\Anaconda2\Scripts\activate.bat C:\Users\root\AppData\Local\conda\conda\envs\python3
+python C:\Users\root\Desktop\WesternUnion\WesternUnion\random_requests.py
